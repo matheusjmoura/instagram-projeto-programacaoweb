da615e9a039f062bb5337ce647bb5fe10a0ee006
Rafael Costanzi <rafaelcostanzi@protonmail.com>
2021-05-21 23:27:53 -0300

    implementa endpoints pessoas e nacionalidade

--- server.js
+++ server.js
@@ -59,5 +59,96 @@ app.delete('/api/usuarios/:id', async (req, res) => {
   res.send(usuarioDeletado);
 });
 
+const Nacionalidade = mongoose.model(
+  'nacionalidade',
+  new mongoose.Schema({
+    _id: { type: String, default: shortid.generate },
+    nat: String,
+  })
+);
+
+app.get('/api/nacionalidade', async (req, res) => {
+  const nat = await Nacionalidade.find({}).sort({ nat: `asc` });
+  res.send(nat);
+});
+
+app.post('/api/nacionalidade', async (req, res) => {
+  if (req.body.length > 1) {
+    const nacionalidades = [];
+    req.body.forEach((element) => {
+      if (!element.nat) {
+        return res.send({ message: 'Os dados são obrigatórios.' });
+      } else {
+        nacionalidades.push(new Nacionalidade(element));
+      }
+    });
+    const nacionalidade = await nacionalidades.forEach((n) => {
+      n.save();
+    });
+    res.send(nacionalidade);
+  } else {
+    if (!req.body.nat) {
+      return res.send({ message: 'Os dados são obrigatórios.' });
+    }
+    const nacionalidade = await Nacionalidade(req.body).save();
+    res.send(nacionalidade);
+  }
+});
+
+const Pessoa = mongoose.model(
+  'pessoas',
+  new mongoose.Schema(
+    {
+      _id: {
+        type: String,
+        default: shortid.generate,
+      },
+      name_first: String,
+      name_last: String,
+      email: String,
+      gender: String,
+      nat: String,
+      dob_date: String,
+      phone: String,
+      picture_thumbnail: String,
+    },
+    {
+      timestamps: true,
+    }
+  )
+);
+
+app.post('/api/pessoas', async (req, res) => {
+  if (req.body.length > 1) {
+    const pessoas = [];
+    req.body.forEach((element) => {
+      if (!element.name_first || !element.email) {
+        return res.send({ message: 'Os dados são obrigatórios.' });
+      }
+      pessoas.push(new Pessoa(element));
+    });
+    const pessoa = await pessoas.forEach((p) => {
+      p.save();
+    });
+    res.send(pessoa);
+  } else {
+    if (!req.body.name_first || !req.body.email) {
+      return res.send({ message: 'Os dados são obrigatórios.' });
+    }
+    const pessoa = await Pessoa(req.body).save();
+    res.send(pessoa);
+  }
+});
+
+app.get('/api/pessoas/find', async (req, res) => {
+  const pessoas = await Pessoa.find(req.query);
+  res.send(pessoas);
+});
+
+app.delete('/api/pessoas/:id', async (req, res) => {
+  const pessoa = await Pessoa.findByIdAndDelete(req.params.id);
+  res.send(pessoa);
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Server em http://localhost:5000'));
